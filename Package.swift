67ebcb1d5dd907f39172ca034395cb315dd025ea
Mehul Parmar <m.parmar@covantex.com>
2024-05-10 19:11:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package (
 //					  path: "./Sources/CovantexSDK.xcframework.zip")
 		.binaryTarget(
 			name: "CovantexSDK",
-			url: "https://github.com/mparmar-covantex/CovantexSDK/releases/1.0.4/CovantexSDK.xcframework.zip",
+			url: "https://github.com/mparmar-covantex/CovantexSDK/releases/download/1.0.5/CovantexSDK.xcframework.zip",
 			checksum: "7c3d745735f4155660c79ba457c7bce13362795ce8b03808a56ec0a5a8e15742"
 		)
 //		.binaryTarget(name: "CovantexSDK",
