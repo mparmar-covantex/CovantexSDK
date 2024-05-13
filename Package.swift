295606267e5da473ca196f8044debb1ab10cb0df
Mehul Parmar <m.parmar@covantex.com>
2024-05-13 11:59:22 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,22 +5,37 @@ import PackageDescription
 let package = Package(
 	name: "CovantexSDK",
 	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v13), .macOS(.v13)
+	],
 	products: [
 		.library(
 			name: "CovantexSDK",
 			targets: ["CovantexSDK", "CovantexFrameworkWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.0")),
+		.package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
+		.package(url: "https://github.com/mparmar-covantex/ObjectMapper.git", .upToNextMajor(from: "4.4.0")),
+		.package(url: "https://github.com/parmar-mehul/KeychainAccess", .upToNextMajor(from: "1.0.2")),
+		.package(url: "https://github.com/parmar-mehul/Mute", .upToNextMajor(from: "1.0.9"))
 	],
 	targets: [
 		.target(
 			name: "CovantexFrameworkWrapper",
 			dependencies: [
 				"CovantexSDK",
+				"Alamofire",
+				"ObjectMapper",
+				"SwiftyJSON",
+				"KeychainAccess",
+				"Mute"
 			],
 			path: "CovantexFrameworkWrapper"
 		),
 		.binaryTarget(
 			name: "CovantexSDK",
-			url: "https://github.com/mparmar-covantex/CovantexSDK/releases/download/1.0.13/CovantexSDK.xcframework.zip",
+			url: "https://github.com/mparmar-covantex/CovantexSDK/releases/download/1.0.14/CovantexSDK.xcframework.zip",
 			checksum: "e286544525df38d33006b4ffde3edceeb89c23f2c1d638da3d32209fda03af30"
 		)
 	]
